2232082acb61dd555febb164ea5f43a129f8b6a6
AnnikaLindberg <annika.edit.lindberg@gmail.com>
2024-04-08 16:14:17 +0200

    imagepicker added

---
+++ code/02-nextjs-essentials/11-populating-starting-page/app/meals/error.js
@@ -0,0 +1,9 @@
+'use client';
+export default function Error() {
+  return (
+    <main className='error'>
+      <h1>An error occurred!</h1>
+      <p>Unfortunately, an error occurred while fetching the data.</p>
+    </main>
+  );
+}
